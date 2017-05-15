8f399809c119654b040e4705eb4b6ebf618fa87e
Dũng Trần <tad88.dev@gmail.com>
2017-05-15 15:10:49 +0700

    Update smart contracts

--- contracts/TheDivine.sol
+++ contracts/TheDivine.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.9;
+pragma solidity ^0.4.11;
 
 contract TheDivine{
 
@@ -10,6 +10,9 @@ contract TheDivine{
 
     /* Total number of randomness in the chain */
     uint public Total;
+
+    /* Complex of digest compute */
+    uint constant Complex = 999;
        
     /**
     * Construct function
@@ -25,24 +28,30 @@ contract TheDivine{
     */
     function GetPower() public returns(bytes32){
         uint Previous = uint(sha3(Immotal[Total-1]));
-        uint PickUp = Previous >> 128 ^ Previous << 128;
+        uint PickUp = (Previous >> 128) ^ (Previous << 128);
         uint ThePast = uint(Immotal[PickUp % Total]);
         uint Shift = PickUp % 256;
-
-        // Swing previous value by shift operator
+        bytes32 Temporary;
+        
+        // Rotate previous value by shift operator
         Previous = (Previous >> Shift) | (Previous << (256 - Shift));
 
-        // Swing ThePast value by shift operator
+        // Reverse rotate ThePast value by shift operator
         ThePast = (ThePast >> (256 - Shift)) | (ThePast << Shift);
         
+        // Calculate digest by Complex times
+        Temporary = sha3(Previous,                   // Previous
+                         ThePast,                    // Random pickup value from the chain
+                         Previous - ThePast,         // Distance
+                         Total,                      // Total number of randomness in the chain
+                         Nonce[msg.sender]++);       // Sender nonce
+
+        for(uint Count = 0; Count < Complex; Count++){
+            Temporary = sha3(Temporary);
+        }
+
         // Append number to the chain
-        Immotal[Total++] = sha3(
-                                Previous,                   // Previous
-                                ThePast,                    // Random pickup value from the chain
-                                Previous - ThePast,         // Distance
-                                Total,                      // Total number of randomness in the chain
-                                Nonce[msg.sender]++         // Sender nonce
-                            );
+        Immotal[Total++] = Temporary;
 
         // Return last number of the chain
         return Immotal[Total-1];

1db8499d8ae0f3f3162fd653b4ed7bf7006cfaed
Chiro Hiro <chiro@fkguru.com>
2018-05-31 23:11:08 +0700

    Add migration script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var TheDivine = artifacts.require("./TheDivine.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer) {
+    deployer.deploy(Migrations);
+    deployer.deploy(TheDivine);
 };

54355bc7b3d3266b7c0d9d743e3f50c583885aaa
Chiro Hiro <chiro@fkguru.com>
2018-05-31 22:37:18 +0700

    Add truffle

---
+++ truffle-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            gas: 3712390,
+            network_id: "*" // Match any network id
+        }
+    }
+};
